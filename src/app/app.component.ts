18ea39756c7d3e36286c905b9559fd59a899ef33
plazadiazmario <plazadiazmario@gmail.com>
2024-04-11 09:55:56 +0200

    pdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  data: any[] = [
+    { id: 1, nombre: 'John', apellido: 'Doe', pdfSrc: "../assets/ejemplo.pdf" },
+    { id: 2, nombre: 'Jane', apellido: 'Smith', pdfSrc: "../assets/ejemplo.pdf" },
+    { id: 1, nombre: 'John', apellido: 'Doe', pdfSrc: "../assets/ejemplo.pdf" },
+    { id: 2, nombre: 'Jane', apellido: 'Smith', pdfSrc: "../assets/ejemplo.pdf" },
+    { id: 1, nombre: 'John', apellido: 'Doe', pdfSrc: "../assets/ejemplo.pdf" },
+    { id: 2, nombre: 'Jane', apellido: 'Smith', pdfSrc: "../assets/ejemplo.pdf" },
+  ];
+
   title = 'pruebaPDF';
+  showPdf: boolean = false;
+  zoomLevel: number = 1.0; // Inicializar el nivel de zoom
+  pdfSrc = "../assets/ejemplo.pdf";
+
+  constructor(){
+    (window as any).pdfWorkerSrc = '../assets/pdf.worker.min.js';
+  }
+  zoomIn() {
+    this.zoomLevel += 0.1; // Incrementar el nivel de zoom
+  }
+
+  zoomOut() {
+    this.zoomLevel -= 0.1; // Decrementar el nivel de zoom
+  }
+  togglePdfViewer() {
+    this.showPdf = !this.showPdf;
+  }
 }
